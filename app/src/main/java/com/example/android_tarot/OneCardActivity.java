eff0f98b5f0f7a0f7264cc13fb381be899d28d68
hiroyuki <idiotpanzer@gmail.com>
2019-07-09 09:45:18 +0900

    Draw tarot card randomly

--- app/src/main/java/com/example/android_tarot/OneCardActivity.java
+++ app/src/main/java/com/example/android_tarot/OneCardActivity.java
@@ -1,6 +1,8 @@
 package com.example.android_tarot;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,13 @@ public class OneCardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one_card);
+
+        // リソース情報からタロットの絵柄をランダムで取得する
+        final TypedArray tarotImagesArray = getResources().obtainTypedArray(R.array.loading_tarot_images);
+        int choice = (int) (Math.random() * tarotImagesArray.length());
+
+        ImageView oneCardImage = (ImageView) findViewById(R.id.oneCardImage);
+        oneCardImage.setImageResource(tarotImagesArray.getResourceId(choice, R.drawable.cu01));
+        tarotImagesArray.recycle();
     }
 }
