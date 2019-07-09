5c308386b9ce577b17cd6b7ba58c092b8383b434
hiroyuki <idiotpanzer@gmail.com>
2019-07-09 14:48:11 +0900

    Display tarot card names also

--- app/src/main/java/com/example/android_tarot/OneCardActivity.java
+++ app/src/main/java/com/example/android_tarot/OneCardActivity.java
@@ -3,6 +3,7 @@ package com.example.android_tarot;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,10 +16,15 @@ public class OneCardActivity extends AppCompatActivity {
 
         // リソース情報からタロットの絵柄をランダムで取得する
         final TypedArray tarotImagesArray = getResources().obtainTypedArray(R.array.loading_tarot_images);
+        final String[] tarotNameArray = getResources().getStringArray(R.array.loading_tarot_names);
+
         int choice = (int) (Math.random() * tarotImagesArray.length());
 
         ImageView oneCardImage = (ImageView) findViewById(R.id.oneCardImage);
         oneCardImage.setImageResource(tarotImagesArray.getResourceId(choice, R.drawable.cu01));
         tarotImagesArray.recycle();
+
+        TextView tarotName = (TextView) findViewById(R.id.tarotName);
+        tarotName.setText(tarotNameArray[choice]);
     }
 }
