31b0830d4d885de50c16cdf88db9da592d36439e
hiroyuki <idiotpanzer@gmail.com>
2019-07-09 09:19:48 +0900

    Add intent and page forwarding

--- app/src/main/java/com/example/android_tarot/OneCardActivity.java
+++ app/src/main/java/com/example/android_tarot/OneCardActivity.java
@@ -1,6 +1,14 @@
 package com.example.android_tarot;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OneCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one_card);
+    }
 }
