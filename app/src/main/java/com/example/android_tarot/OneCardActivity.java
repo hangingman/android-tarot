55aafd8624ef7dc15d35133be3e69219e4826006
hiroyuki nagata <newserver002@gmail.com>
2019-07-13 19:37:48 +0900

    Enable to show reading result of positive/reverse

--- app/src/main/java/com/example/android_tarot/OneCardActivity.java
+++ app/src/main/java/com/example/android_tarot/OneCardActivity.java
@@ -17,6 +17,8 @@ public class OneCardActivity extends AppCompatActivity {
         // リソース情報からタロットの絵柄をランダムで取得する
         final TypedArray tarotImagesArray = getResources().obtainTypedArray(R.array.loading_tarot_images);
         final String[] tarotNameArray = getResources().getStringArray(R.array.loading_tarot_names);
+        final String[] readingPositive = getResources().getStringArray(R.array.tarot_reading_positive);
+        final String[] readingReverse = getResources().getStringArray(R.array.tarot_reading_reverse);
 
         int choice = (int) (Math.random() * tarotImagesArray.length());
 
@@ -25,8 +27,10 @@ public class OneCardActivity extends AppCompatActivity {
         // 1/2の確率で逆位置
         // Math.random() returns a value between 0.0 and 1.0
         // so it is heads or tails 50% of the time
+        boolean isReverse = false;
         if (Math.random() < 0.5) {
             oneCardImage.setRotation(180);
+            isReverse = true;
         }
 
         oneCardImage.setImageResource(tarotImagesArray.getResourceId(choice, R.drawable.cu01));
@@ -34,5 +38,12 @@ public class OneCardActivity extends AppCompatActivity {
 
         TextView tarotName = (TextView) findViewById(R.id.tarotName);
         tarotName.setText(tarotNameArray[choice]);
+
+        TextView tarotDesc = (TextView) findViewById(R.id.tarotDescTextView);
+        if (isReverse) {
+            tarotDesc.setText(readingReverse[choice]);
+        } else {
+            tarotDesc.setText(readingPositive[choice]);
+        }
     }
 }
